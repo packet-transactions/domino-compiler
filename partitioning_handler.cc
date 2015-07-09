ad73d6865471539b595dd1e4830eed5bbaab87e4
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-07-09 13:42:58 -0700

    Partition into a pipeline, finally!

--- partitioning_handler.cc
+++ partitioning_handler.cc
@@ -12,7 +12,7 @@ void PartitioningHandler::run(const MatchFinder::MatchResult & t_result) {
   const FunctionDecl *function_decl_expr = t_result.Nodes.getNodeAs<clang::FunctionDecl>("functionDecl");
   assert(function_decl_expr != nullptr);
   assert(isa<CompoundStmt>(function_decl_expr->getBody()));
-  std::vector<const BinaryOperator *> useful_ops;
+  InstructionVector useful_ops;
   for (const auto & child : function_decl_expr->getBody()->children()) {
     assert(child);
     assert(isa<DeclStmt>(child) or isa<BinaryOperator>(child));
@@ -24,21 +24,16 @@ void PartitioningHandler::run(const MatchFinder::MatchResult & t_result) {
     }
   }
 
-  std::map<const BinaryOperator *, std::vector<const BinaryOperator *>> succ_graph;
-  std::map<const BinaryOperator *, std::vector<const BinaryOperator *>> pred_graph;
-  for (const auto & op : useful_ops) {
-    succ_graph[op] = {};
-    pred_graph[op] = {};
-  }
+  // Partition into a pipeline
+  auto schedule = partition(useful_ops);
 
-  for (uint32_t i = 0; i < useful_ops.size(); i++) {
-    for (uint32_t j = i + 1; j < useful_ops.size(); j++) {
-      if (depends(useful_ops.at(i), useful_ops.at(j))) {
-        // edge from i ---> j
-        succ_graph.at(useful_ops.at(i)).emplace_back(useful_ops.at(j));
-        pred_graph.at(useful_ops.at(j)).emplace_back(useful_ops.at(i));
-      }
+  // Print out schedule
+  for (uint32_t i = 0; i < schedule.size(); i++) {
+    std::cout << "Clock " << i << " : " << std::endl;
+    for (const auto & op : schedule.at(i)) {
+      std::cout << " { " << clang_stmt_printer(op) << " } " << " ";
     }
+    std::cout << std::endl;
   }
 }
 
@@ -49,6 +44,13 @@ bool PartitioningHandler::op_reads_var(const BinaryOperator * op, const DeclRefE
 }
 
 bool PartitioningHandler::depends(const BinaryOperator * op1, const BinaryOperator * op2) const {
+  // We are being a little conservative here and flagging all 
+  // three "textbook" dependencies:
+  // Read After Write,
+  // Write After Write,
+  // and Write After Read.
+  // Although renaming can prevent the latter two.
+
   // assume and check that op1 precedes op2 in program order
   assert(op1->getLocStart() < op2->getLocStart());
   assert(isa<DeclRefExpr>(op1->getLHS()));
@@ -70,4 +72,64 @@ bool PartitioningHandler::depends(const BinaryOperator * op1, const BinaryOperat
   }
 
   return false;
+}
+
+PartitioningHandler::InstructionSchedule PartitioningHandler::partition(const InstructionVector & inst_vector) const {
+  // Create dag of instruction dependencies.
+  std::map<const BinaryOperator *, InstructionVector> succ_graph;
+  std::map<const BinaryOperator *, InstructionVector> pred_graph;
+  for (const auto & op : inst_vector) {
+    succ_graph[op] = {};
+    pred_graph[op] = {};
+  }
+
+  for (uint32_t i = 0; i < inst_vector.size(); i++) {
+    for (uint32_t j = i + 1; j < inst_vector.size(); j++) {
+      if (depends(inst_vector.at(i), inst_vector.at(j))) {
+        // edge from i ---> j
+        succ_graph.at(inst_vector.at(i)).emplace_back(inst_vector.at(j));
+        pred_graph.at(inst_vector.at(j)).emplace_back(inst_vector.at(i));
+      }
+    }
+  }
+
+  // Keep track of what needs to be scheduled
+  InstructionVector to_schedule = inst_vector;
+
+  // The schedule itself
+  InstructionSchedule schedule;
+
+  while (not to_schedule.empty()) {
+    // Find elements with no predecessors
+    std::vector<const BinaryOperator *> chosen_elements;
+    for (const auto & candidate : to_schedule) {
+      if (pred_graph.at(candidate).empty()) {
+        chosen_elements.emplace_back(candidate);
+      }
+    }
+    assert(not chosen_elements.empty());
+
+    // append to schedule
+    schedule.emplace_back(chosen_elements);
+
+    // remove chosen_elements from graph by deleting it
+    for (const auto & chosen_element : chosen_elements) {
+      // : from all its successors' pred_graph
+      // : from all its predessors's succ_graph (trivially true, because it has no predessors)
+      for (const auto & successor : succ_graph.at(chosen_element)) {
+        // delete chosen_element from pred_graph.at(successor)
+        pred_graph.at(successor).erase(std::remove(pred_graph.at(successor).begin(), pred_graph.at(successor).end(), chosen_element));
+      }
+
+      // Delete everything from succ_graph.at(chosen_element)
+      succ_graph.at(chosen_element).clear();
+    }
+
+    // remove chosen_elements from to_schedule
+    for (const auto & chosen_element : chosen_elements) {
+      to_schedule.erase(std::remove(to_schedule.begin(), to_schedule.end(), chosen_element));
+    }
+  }
+
+  return schedule;
 }
--- partitioning_handler.h
+++ partitioning_handler.h
@@ -22,12 +22,24 @@ class PartitioningHandler : public clang::ast_matchers::MatchFinder::MatchCallba
   virtual void run(const clang::ast_matchers::MatchFinder::MatchResult & t_result) override;
 
  private:
+  /// Convenience typedef: A vector of instructions, in our hyper-restrictive
+  /// language everything is a BinaryOperator
+  typedef std::vector<const clang::BinaryOperator*> InstructionVector;
+
+  /// Convenience typedef: A vector of vectors representing instructions scheduled
+  /// at each time slot or clock
+  typedef std::vector<InstructionVector> InstructionSchedule;
+
   /// Does operation read variable?
   bool op_reads_var(const clang::BinaryOperator * op, const clang::DeclRefExpr * var) const;
 
   /// Is there a dependence _from_ BinaryOperator op1 _to_ BinaryOperator op2?
   /// Returns true if op1 MUST precede op 2
   bool depends(const clang::BinaryOperator * op1, const clang::BinaryOperator * op2) const;
+
+  /// Core partitioning logic
+  /// Build a dag of dependencies and then schedule using the DAG
+  InstructionSchedule partition(const InstructionVector & inst_vector) const;
 };
 
 #endif  // PARTITIONING_HANDLER_H_
