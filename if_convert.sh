56505936e24ec9122d0b5e25f41a8602e11e61fb
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-07-09 19:00:36 -0700

    Simplify if_convert.sh

--- if_convert.sh
+++ if_convert.sh
@@ -7,20 +7,4 @@ set -v
 ./struct_to_local_vars $1 -- > repl.yaml
 clang-apply-replacements .
 
-# Iterate till a fixed point
-while [ 1 -lt 2 ] ;
-do
-  cp $1 old.cc
-  ./if_conversion_prog $1 -- > repl.yaml
-  clang-apply-replacements .
-  cp $1 new.cc
-  if diff old.cc new.cc;
-  then
-    break;
-  else
-    continue;
-  fi
-done
-
-rm repl.yaml old.cc new.cc
-clang-format-3.5 -i $1
+./if_conversion_prog $1 -- | clang-format-3.5
