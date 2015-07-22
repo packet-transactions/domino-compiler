5b31eb0e1fe9c0d014ba96072a194254f1ec7642
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-07-22 12:26:01 -0700

    Autoconf

--- clang_compile.sh
+++
@@ -1,5 +0,0 @@
-g++ --std=c++14 -isystem /usr/lib/llvm-3.5/include/ \
-	-pedantic -Wconversion -Wsign-conversion -Wall -Wextra -Weffc++ -Werror -fno-default-inline \
-	$@ \
-  libclangall.a \
-	`llvm-config --ldflags --libs --system-libs`
