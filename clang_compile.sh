0413a0a2a48164c985812850933d7bcdf116bbe3
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-07-22 12:10:39 -0700

    Create libclangall.a for all clang related stuff.
    so we can move to autotools

--- clang_compile.sh
+++ clang_compile.sh
@@ -1,22 +1,5 @@
 g++ --std=c++14 -isystem /usr/lib/llvm-3.5/include/ \
 	-pedantic -Wconversion -Wsign-conversion -Wall -Wextra -Weffc++ -Werror -fno-default-inline \
 	$@ \
-	-L /usr/lib/llvm-3.5/lib/  \
-	-Wl,--start-group \
-	-lclangAST \
-	-lclangAnalysis \
-	-lclangBasic \
-	-lclangDriver \
-	-lclangEdit \
-	-lclangFrontend \
-	-lclangFrontendTool \
-	-lclangLex \
-	-lclangParse \
-	-lclangSema \
-	-lclangASTMatchers \
-	-lclangRewrite \
-	-lclangRewriteFrontend \
-	-lclangSerialization \
-	-lclangTooling \
-	-Wl,--end-group \
+  libclangall.a \
 	`llvm-config --ldflags --libs --system-libs`
---
+++ create_clang_lib.sh
@@ -0,0 +1,64 @@
+set -v
+set -x
+
+# Clean out all previous work
+rm -rf obj_files all_objs
+mkdir obj_files
+mkdir all_objs
+
+# Iterate through all .a files
+for file in /usr/lib/llvm-3.5/lib/libclangAST.a \
+            /usr/lib/llvm-3.5/lib/libclangAnalysis.a \
+            /usr/lib/llvm-3.5/lib/libclangBasic.a \
+            /usr/lib/llvm-3.5/lib/libclangDriver.a \
+            /usr/lib/llvm-3.5/lib/libclangEdit.a \
+            /usr/lib/llvm-3.5/lib/libclangFrontend.a \
+            /usr/lib/llvm-3.5/lib/libclangFrontendTool.a \
+            /usr/lib/llvm-3.5/lib/libclangLex.a\
+            /usr/lib/llvm-3.5/lib/libclangParse.a \
+            /usr/lib/llvm-3.5/lib/libclangSema.a\
+            /usr/lib/llvm-3.5/lib/libclangASTMatchers.a\
+            /usr/lib/llvm-3.5/lib/libclangRewrite.a\
+            /usr/lib/llvm-3.5/lib/libclangRewriteFrontend.a\
+            /usr/lib/llvm-3.5/lib/libclangSerialization.a\
+            /usr/lib/llvm-3.5/lib/libclangTooling.a
+do
+  file_name=`echo $file | cut -d '/' -f 6`
+  echo "file_name is $file_name"
+  mkdir obj_files/$file_name
+  cd obj_files/$file_name
+  ar x $file
+  cd ../..
+done
+
+for file in /usr/lib/llvm-3.5/lib/libclangAST.a \
+            /usr/lib/llvm-3.5/lib/libclangAnalysis.a \
+            /usr/lib/llvm-3.5/lib/libclangBasic.a \
+            /usr/lib/llvm-3.5/lib/libclangDriver.a \
+            /usr/lib/llvm-3.5/lib/libclangEdit.a \
+            /usr/lib/llvm-3.5/lib/libclangFrontend.a \
+            /usr/lib/llvm-3.5/lib/libclangFrontendTool.a \
+            /usr/lib/llvm-3.5/lib/libclangLex.a\
+            /usr/lib/llvm-3.5/lib/libclangParse.a \
+            /usr/lib/llvm-3.5/lib/libclangSema.a\
+            /usr/lib/llvm-3.5/lib/libclangASTMatchers.a\
+            /usr/lib/llvm-3.5/lib/libclangRewrite.a\
+            /usr/lib/llvm-3.5/lib/libclangRewriteFrontend.a\
+            /usr/lib/llvm-3.5/lib/libclangSerialization.a\
+            /usr/lib/llvm-3.5/lib/libclangTooling.a
+do
+  file_name=`echo $file | cut -d '/' -f 6`
+  echo "file_name is $file_name"
+  cd obj_files/$file_name
+  for obj in *.o
+  do
+     mv $obj ../../all_objs/$file_name-$obj
+  done
+  cd ../..
+done
+
+cd all_objs
+ar qc libclangall.a *.o
+mv libclangall.a ..
+
+rm -rf all_objs obj_files
