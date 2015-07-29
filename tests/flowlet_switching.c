a6345b95302454a7e63c2e3dcff1eaccbc33b2ba
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-07-29 13:33:31 -0700

    More comprehensive flowlet switching example

--- tests/flowlet_switching.c
+++ tests/flowlet_switching.c
@@ -1,8 +1,11 @@
+#define NUM_HOPS 10
 #define THRESHOLD 5 // Flowlet gap: TODO, should be something configurable
 
 typedef struct Packet {
-  int new_next_hop;
-  int time;
+  int sport;
+  int dport;
+  int new_hop;
+  int arrival_time;
   int next_hop;
 } Packet;
 
@@ -10,10 +13,10 @@ int last_time;
 int next_hop;
 
 void func(Packet pkt) {
-  pkt.new_next_hop = 2; // TODO: 2 should technically be rand()
-  if (pkt.time - last_time > THRESHOLD) {
-    next_hop = pkt.new_next_hop;
+  pkt.new_hop = (pkt.sport * pkt.dport) % NUM_HOPS;
+  if (pkt.arrival_time - last_time > THRESHOLD) {
+    next_hop = pkt.new_hop;
   }
-  last_time = pkt.time;
+  last_time = pkt.arrival_time;
   pkt.next_hop = next_hop;
 }
