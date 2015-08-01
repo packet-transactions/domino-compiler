97387c1412b875f7959680004a9d83c595cd173d
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-07-31 18:43:53 -0700

    Use SinglePass for expr_propagater

--- expr_propagater.cc
+++ expr_propagater.cc
@@ -1,48 +1,90 @@
 #include <iostream>
-#include <set>
 #include <string>
+#include "clang/AST/AST.h"
 #include "clang/Tooling/Refactoring.h"
 #include "clang/Tooling/CommonOptionsParser.h"
-#include "run_pass.h"
-#include "func_transform_handler.cc"
-#include "packet_decl_creator.h"
-#include "state_var_decl_handler.h"
-#include "expr_propagation_handler.h"
-#include "func_decl_pass_through.h"
+#include "clang_utility_functions.h"
+#include "single_pass.h"
 
+using namespace clang;
 using namespace clang::tooling;
 
-static llvm::cl::OptionCategory strength_redux(""
-"Simple strength reduction: rewrite if (1) ? x : y to x."
-"Simplify 1 && x to x");
+/// The actual expression propagation
+static std::string expr_prop_helper(const FunctionDecl* function_decl) {
+  assert(function_decl);
+
+  // Maintain map from variable name (packet or state variable)
+  // to a string representing its expression, for expression propagation.
+  std::map<std::string, std::string> var_to_expr;
+
+  // Rewrite function body
+  std::string transformed_body = "";
+  const auto * function_body = function_decl->getBody();
+  assert(isa<CompoundStmt>(function_body));
+  for (const auto & child : function_body->children()) {
+    assert(isa<BinaryOperator>(child));
+    const auto * bin_op = dyn_cast<BinaryOperator>(child);
+    assert(bin_op->isAssignmentOp());
+
+    // Strip off parenthesis and casts for LHS and RHS
+    const auto * rhs = bin_op->getRHS()->IgnoreParenImpCasts();
+    const auto * lhs = bin_op->getLHS()->IgnoreParenImpCasts();
+
+    // Populate var_to_expr
+    var_to_expr[clang_stmt_printer(lhs)] = clang_stmt_printer(rhs);
+
+    if ((isa<DeclRefExpr>(rhs) or isa<MemberExpr>(rhs)) and (var_to_expr.find(clang_stmt_printer(rhs)) != var_to_expr.end())) {
+      // If rhs is a packet/state variable, replace it with its current expr
+      transformed_body += clang_stmt_printer(lhs) + "=" + var_to_expr.at(clang_stmt_printer(rhs)) + ";";
+    } else {
+      // Pass through
+      transformed_body += clang_stmt_printer(lhs) + "=" + clang_stmt_printer(rhs) + ";";
+    }
+  }
+
+  // Append function body to signature
+  assert(function_decl->getNumParams() >= 1);
+  const auto * pkt_param = function_decl->getParamDecl(0);
+  const auto pkt_type  = function_decl->getParamDecl(0)->getType().getAsString();
+  const auto pkt_name = clang_value_decl_printer(pkt_param);
+
+  // Get transformed_body string
+  return function_decl->getReturnType().getAsString() + " " +
+         function_decl->getNameInfo().getName().getAsString() +
+         "( " + pkt_type + " " +  pkt_name + ") { " +
+         transformed_body + "}\n";
+}
+
+/// Expression propagation transformation
+static std::string expr_prop_transform(const TranslationUnitDecl * tu_decl) {
+  std::string ret;
+  // Loop through all declarations within the translation unit decl
+  for (const auto * child_decl : dyn_cast<DeclContext>(tu_decl)->decls()) {
+    assert(child_decl);
+    if (isa<VarDecl>(child_decl) or
+        isa<RecordDecl>(child_decl) or
+        (isa<FunctionDecl>(child_decl) and (not is_packet_func(dyn_cast<FunctionDecl>(child_decl))))) {
+      ret += clang_decl_printer(child_decl) + ";";
+    } else if (isa<FunctionDecl>(child_decl) and (is_packet_func(dyn_cast<FunctionDecl>(child_decl)))) {
+      ret += expr_prop_helper(dyn_cast<FunctionDecl>(child_decl));
+    }
+  }
+  return ret;
+}
+
+
+
+static llvm::cl::OptionCategory expr_prop(""
+"Expr propagation: replace y = b + c; a = y;"
+"with y=b+c; a=b+c; In some sense we are inverting"
+"the process of common subexpression elimination");
 
 int main(int argc, const char **argv) {
   // Set up parser options for refactoring tool
-  CommonOptionsParser op(argc, argv, strength_redux);
+  CommonOptionsParser op(argc, argv, expr_prop);
 
-  // Run passes, chaining results if required
-  const auto state_vars     = run_pass<StateVarDeclHandler,
-                                      std::string>
-                                      (op, clang::ast_matchers::decl().bind("decl"));
-
-  const auto func_decls     = run_pass<FuncDeclPassThrough,
-                                      std::string>
-                                      (op, clang::ast_matchers::decl().bind("decl"));
-
-
-  const auto packet_decls   = run_pass<PacketDeclCreator,
-                                      std::string>
-                                      (op, clang::ast_matchers::decl().bind("decl"),
-                                      std::vector<std::string>());
-
-  const auto strength_redux = run_pass<FuncTransformHandler<ExprPropagationHandler>,
-                                      std::pair<std::string, std::vector<std::string>>>
-                                      (op, clang::ast_matchers::functionDecl().bind("functionDecl"));
-  // Print out outputs in sequence
-  std::cout << state_vars << std::endl
-            << packet_decls << std::endl
-            << func_decls << std::endl
-            << strength_redux.first << std::endl;
+  // Parse file once and output it
+  std::cout << SinglePass(op, expr_prop_transform).output();
 
   return 0;
 }
--- strength_reducer.cc
+++ strength_reducer.cc
@@ -1,12 +1,109 @@
 #include <iostream>
-#include <set>
 #include <string>
+#include "clang/AST/AST.h"
 #include "clang/Tooling/Refactoring.h"
 #include "clang/Tooling/CommonOptionsParser.h"
+#include "clang_utility_functions.h"
 #include "single_pass.h"
-#include "strength_reducer_transform.h"
 
+using namespace clang;
 using namespace clang::tooling;
+
+/// The actual strength reduction
+static std::string strength_reduce_helper(const FunctionDecl* function_decl) {
+  assert(function_decl);
+
+  // Rewrite function body
+  std::string transformed_body = "";
+  const auto * function_body = function_decl->getBody();
+  assert(isa<CompoundStmt>(function_body));
+  for (const auto & child : function_body->children()) {
+    assert(isa<BinaryOperator>(child));
+    const auto * bin_op = dyn_cast<BinaryOperator>(child);
+    assert(bin_op->isAssignmentOp());
+
+    // Strip off parenthesis and casts for RHS
+    const auto * rhs = bin_op->getRHS()->IgnoreParenImpCasts();
+
+    if(not isa<ConditionalOperator>(rhs)) {
+      // If it isn't a conditional operator, pass it through
+      transformed_body += clang_stmt_printer(bin_op) + ";";
+    } else {
+      const auto * cond_op = dyn_cast<ConditionalOperator>(rhs);
+
+      // Get condition expression
+      const auto * cond = cond_op->getCond()->IgnoreParenImpCasts();
+
+      if(isa<IntegerLiteral>(cond)) {
+        // TODO: Check that it's the integer literal one
+        // replace with straight-up assignment
+        transformed_body += clang_stmt_printer(bin_op->getLHS()) + " = " + clang_stmt_printer(cond_op->getTrueExpr()) + ";"; 
+      } else if (isa<BinaryOperator>(cond)) {
+        // Has to be an &&, otherwise assert
+        const auto * cond_predicate = dyn_cast<BinaryOperator>(cond);
+        assert(BinaryOperator::getOpcodeStr(cond_predicate->getOpcode()) == "&&");
+
+        // Assuming it's &&, get left and right components
+        const auto * left_bool_op = cond_predicate->getLHS();
+        const auto * right_bool_op = cond_predicate->getRHS();
+
+        // If both left_bool_op and right_bool_op are IntegerLiteral, something's wrong
+        assert (not (isa<IntegerLiteral>(left_bool_op) and isa<IntegerLiteral>(right_bool_op)));
+
+        // If either of the two is an IntegerLiteral, return the other
+        // TODO: Check that it's actually 1.
+        const Expr * simplified_pred = nullptr;
+        if (isa<IntegerLiteral>(left_bool_op)) {
+          simplified_pred = right_bool_op;
+        } else if (isa<IntegerLiteral>(right_bool_op)) {
+          simplified_pred = left_bool_op;
+        } else {
+          simplified_pred = cond_predicate;
+        }
+
+        transformed_body += clang_stmt_printer(bin_op->getLHS()) + " = " + clang_stmt_printer(simplified_pred)
+                                                       + " ? " + clang_stmt_printer(cond_op->getTrueExpr())
+                                                       + " : " + clang_stmt_printer(cond_op->getFalseExpr()) + ";";
+      } else if (isa<DeclRefExpr>(cond) or isa<MemberExpr>(cond)) {
+        // pass it through, nothing to simplify here
+        transformed_body += clang_stmt_printer(bin_op) + ";";
+      } else {
+        assert(false);
+      }
+    }
+  }
+
+  // Append function body to signature
+  assert(function_decl->getNumParams() >= 1);
+  const auto * pkt_param = function_decl->getParamDecl(0);
+  const auto pkt_type  = function_decl->getParamDecl(0)->getType().getAsString();
+  const auto pkt_name = clang_value_decl_printer(pkt_param);
+
+  // Get transformed_body string
+  return function_decl->getReturnType().getAsString() + " " +
+         function_decl->getNameInfo().getName().getAsString() +
+         "( " + pkt_type + " " +  pkt_name + ") { " +
+         transformed_body + "}\n";
+}
+
+/// Strength Reduction transformation
+static std::string strength_reducer_transform(const TranslationUnitDecl * tu_decl) {
+  std::string ret;
+  // Loop through all declarations within the translation unit decl
+  for (const auto * child_decl : dyn_cast<DeclContext>(tu_decl)->decls()) {
+    assert(child_decl);
+    if (isa<VarDecl>(child_decl) or
+        isa<RecordDecl>(child_decl) or
+        (isa<FunctionDecl>(child_decl) and (not is_packet_func(dyn_cast<FunctionDecl>(child_decl))))) {
+      ret += clang_decl_printer(child_decl) + ";";
+    } else if (isa<FunctionDecl>(child_decl) and (is_packet_func(dyn_cast<FunctionDecl>(child_decl)))) {
+      ret += strength_reduce_helper(dyn_cast<FunctionDecl>(child_decl));
+    }
+  }
+  return ret;
+}
+
+
 
 static llvm::cl::OptionCategory strength_redux(""
 "Simple strength reduction: rewrite if (1) ? x : y to x."
