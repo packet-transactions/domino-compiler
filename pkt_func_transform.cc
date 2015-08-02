09605defd7888d8965d061c6a4626d3bb540bc1b
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-02 16:47:08 -0700

    Assign an order to each type of declaration

--- pkt_func_transform.cc
+++ pkt_func_transform.cc
@@ -1,12 +1,36 @@
+#include <algorithm>
 #include "pkt_func_transform.h"
 #include "clang_utility_functions.h"
 
 using namespace clang;
 
-std::string pkt_func_transform(const TranslationUnitDecl * tu_decl, const std::function<std::pair<std::string, std::vector<std::string>>(const CompoundStmt *)> & func_body_transform) {
+int get_order(const Decl * decl) {
+  if (isa<VarDecl>(decl)) return 1;
+  else if (isa<RecordDecl>(decl)) return 2;
+  else if (isa<FunctionDecl>(decl) and (not is_packet_func(dyn_cast<FunctionDecl>(decl)))) return 3;
+  else if (isa<FunctionDecl>(decl) and (is_packet_func(dyn_cast<FunctionDecl>(decl)))) return 4;
+  else if (isa<TypedefDecl>(decl)) return 5;
+  else {assert(false); return -1; }
+}
+
+std::string pkt_func_transform(const TranslationUnitDecl * tu_decl,
+                               const std::function<std::pair<std::string, std::vector<std::string>>(const CompoundStmt *)> & func_body_transform) {
+  // String that is returned
   std::string ret;
+
+  // Accumulate all declarations
+  std::vector<const Decl*> all_decls;
+  for (const auto * decl : dyn_cast<DeclContext>(tu_decl)->decls())
+    all_decls.emplace_back(decl);
+
+  // Sort all_decls
+  std::sort(all_decls.begin(),
+            all_decls.end(),
+            [] (const auto * decl1, const auto * decl2)
+            { return get_order(decl1) < get_order(decl2); });
+
   // Loop through all declarations within the translation unit decl
-  for (const auto * child_decl : dyn_cast<DeclContext>(tu_decl)->decls()) {
+  for (const auto * child_decl : all_decls) {
     assert(child_decl);
     if (isa<VarDecl>(child_decl) or
         isa<RecordDecl>(child_decl) or
