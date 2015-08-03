ab08c7008545bd911b126fb01da3c2d9d2106364
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-02 20:37:51 -0700

    Convert expr_flattener to single_pass / pkt_func_transform

--- expr_flattener.cc
+++ expr_flattener.cc
@@ -4,12 +4,10 @@
 #include "clang/Tooling/Refactoring.h"
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "run_pass.h"
-#include "func_transform_handler.cc"
-#include "state_var_decl_handler.h"
-#include "packet_decl_creator.h"
 #include "expr_flattener_handler.h"
 #include "packet_variable_census.h"
-#include "func_decl_pass_through.h"
+#include "pkt_func_transform.h"
+#include "single_pass.h"
 
 using namespace clang::tooling;
 
@@ -22,32 +20,14 @@ int main(int argc, const char **argv) {
   CommonOptionsParser op(argc, argv, expr_flattener);
 
   // Run passes, chaining results if required
-  const auto state_vars           = run_pass<StateVarDeclHandler,
-                                            std::string>
-                                            (op, clang::ast_matchers::decl().bind("decl"));
-
-  const auto func_decls           = run_pass<FuncDeclPassThrough,
-                                            std::string>
-                                            (op, clang::ast_matchers::decl().bind("decl"));
-
   const auto packet_var_set       = run_pass<PacketVariableCensus,
                                             std::set<std::string>>
                                             (op, clang::ast_matchers::decl().bind("decl"));
 
-  const auto prog_decl_pair       = run_pass<FuncTransformHandler<ExprFlattenerHandler>,
-                                            std::pair<std::string, std::vector<std::string>>>
-                                            (op, clang::ast_matchers::functionDecl().bind("functionDecl"),
-                                            packet_var_set);
+  ExprFlattenerHandler expr_flattener_handler(packet_var_set);
+  // Parse file once and output it
+  const FuncBodyTransform expr_flattener = std::bind(& ExprFlattenerHandler::transform, expr_flattener_handler, std::placeholders::_1, std::placeholders::_2);
+  std::cout << SinglePass(op, std::bind(pkt_func_transform, std::placeholders::_1, expr_flattener)).output();
 
-  const auto final_packet_decls   = run_pass<PacketDeclCreator,
-                                            std::string>
-                                            (op, clang::ast_matchers::decl().bind("decl"),
-                                            prog_decl_pair.second);
-
-  // Print out outputs in sequence
-  std::cout << state_vars << std::endl
-            << func_decls << std::endl
-            << final_packet_decls << std::endl
-            << prog_decl_pair.first << std::endl;
-
+  return 0;
 }
