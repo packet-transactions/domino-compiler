e2d66c21a03cb19408a5deee17198f82fdd3d818
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-04 15:22:08 -0700

    More succinct program

--- echo.cc
+++ echo.cc
@@ -7,17 +7,14 @@
 using namespace clang;
 using namespace clang::tooling;
 
-static auto echo_transform(const TranslationUnitDecl * tu_decl) { return clang_decl_printer(tu_decl); }
-
 static llvm::cl::OptionCategory echo_pass(""
 "Driver program for single pass program that takes"
 "in a file and echoes it out as such");
-
 
 int main(int argc, const char **argv) {
   // Set up parser options for refactoring tool
   CommonOptionsParser op(argc, argv, echo_pass);
 
   // Parse file once and output it as such. This is just a test program
-  std::cout << SinglePass<std::string>(op, echo_transform).output();
+  std::cout << SinglePass<std::string>(op, clang_decl_printer).output();
 }
