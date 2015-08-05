0bb9ee9f18bd2d27aecfe4a76e8254be9c62543b
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-05 11:18:22 -0700

    More interpretable temporary names

--- unique_var_generator.cc
+++ unique_var_generator.cc
@@ -1,15 +1,19 @@
 #include <cassert>
 #include "unique_var_generator.h"
 
-std::string UniqueVarGenerator::get_unique_var() const {
-  // Propose tmp_x, where x starts at var_suffix_ + 1
-  // and keeps incrementing until at least some tmp_x
+std::string UniqueVarGenerator::get_unique_var(const std::string & prefix) const {
+  // Propose prefix_x,
+  // where x starts at var_suffix_.at(prefix) + 1
+  // and keeps incrementing until at least some prefix_x
   // is unique and does not belong to var_set_
-  var_suffix_ = var_suffix_ + 1;
-  std::string candidate = "tmp" + std::to_string(var_suffix_);
+  if (var_suffix_.find(prefix) == var_suffix_.end()) {
+    var_suffix_[prefix] = -1;
+  }
+  var_suffix_.at(prefix)++;
+  std::string candidate = prefix + std::to_string(var_suffix_.at(prefix));
   while (var_set_.find(candidate) != var_set_.end()) {
-    var_suffix_++;
-    candidate = "tmp" + std::to_string(var_suffix_);
+    var_suffix_.at(prefix)++;
+    candidate = prefix + std::to_string(var_suffix_.at(prefix));
   }
   assert(var_set_.find(candidate) == var_set_.end());
   var_set_.emplace(candidate);
--- unique_var_generator.h
+++ unique_var_generator.h
@@ -1,6 +1,7 @@
 #ifndef UNIQUE_VAR_GENERATOR_H_
 #define UNIQUE_VAR_GENERATOR_H_
 
+#include <map>
 #include <set>
 #include <string>
 
@@ -10,15 +11,15 @@ class UniqueVarGenerator {
   UniqueVarGenerator(const std::set<std::string> & initial_set) : var_set_(initial_set) {}
 
   /// Get unique variable
-  std::string get_unique_var() const;
+  std::string get_unique_var(const std::string & prefix = "tmp") const;
 
  private:
   /// Set of packet variables names,
   /// passed to the function in constructor
   mutable std::set<std::string> var_set_;
 
-  /// Last unique variable suffix handed out,
-  mutable int var_suffix_ = -1;
+  /// Last unique variable suffix handed out for every prefix
+  mutable std::map<std::string, int>  var_suffix_ = {};
 };
 
 #endif  // UNIQUE_VAR_GENERATOR_H_
