f5b7fe6bdbec56ae0470bf7e1955c33ff7c50325
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-08 17:50:45 -0700

    Add latest passes to compile.sh; verify that it does the right thing on flowlet_switching.c

--- compile.sh
+++ compile.sh
@@ -23,11 +23,17 @@ done
 # Propagate expressions to their uses
 ./expr_propagater /tmp/flat.c -- > /tmp/prop.c
 
-# Run partitioner on /tmp/prop.c
-./partitioner /tmp/prop.c -- > /tmp/partition.txt
+# Run stateful_flanks on /tmp/prop.c to add prologue and epilpogue for state vars
+./stateful_flanks /tmp/prop.c -- > /tmp/stateful_flanks.c
+
+# Run ssa on /tmp/stateful_flanks.c to generate Stateful Static Single Assignment
+./ssa /tmp/stateful_flanks.c -- > /tmp/ssa.c
+
+# Run dep_graph on /tmp/ssa.c to generate final dependency graph after condensation
+./dep_graph /tmp/ssa.c -- 2> /tmp/dep.dot
 
 echo "Original: "
 cat $1
 echo "************"
-echo "Partitioned: "
-cat /tmp/partition.txt
+echo "Dep graph: "
+cat /tmp/dep.dot | dot -T png | display
