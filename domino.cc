1ff433f4600e6bfc863898fbd148a82481efbc24
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-10 23:37:35 -0700

    Ok, the best I have in terms of declaratively specifying passes
    There is some redundancy because of the enum, but I can live with it.

--- domino.cc
+++ domino.cc
@@ -6,6 +6,7 @@
 #include "expr_flattener_handler.h"
 #include "strength_reducer.h"
 
+#include <utility>
 #include <iostream>
 #include <set>
 #include <string>
@@ -23,25 +24,28 @@ using std::placeholders::_1;
 using std::placeholders::_2;
 
 // Vector of Transform functions, each transform function runs within a SinglePass
-typedef  std::function<std::string(const clang::TranslationUnitDecl *)> Transform;
+enum class TransformType { SINGLE, FIXED_POINT };
+typedef  std::pair<TransformType, std::function<std::string(const clang::TranslationUnitDecl *)>> Transform;
 typedef  std::vector<Transform> TransformVector;
 
 int main(int argc, const char **argv) {
   // Get string that needs to be parsed
   const auto string_to_parse = file_to_str(get_file_name(argc, argv));
 
-  // Parse file once and output if converted version
-  const auto if_convert_output = SinglePass<std::string>(std::bind(& IfConversionHandler::transform, IfConversionHandler(), _1))(string_to_parse);
+  // List out all passes
+  TransformVector transforms = { std::make_pair(TransformType::SINGLE, std::bind(& IfConversionHandler::transform, IfConversionHandler(), _1)),
+                                 std::make_pair(TransformType::SINGLE, strength_reducer_transform),
+                                 std::make_pair(TransformType::FIXED_POINT, ExprFlattenerHandler::transform),
+                                 std::make_pair(TransformType::SINGLE, expr_prop_transform),
+                                 std::make_pair(TransformType::SINGLE, stateful_flank_transform),
+                                 std::make_pair(TransformType::SINGLE, ssa_transform),
+                                 std::make_pair(TransformType::SINGLE, partitioning_transform) };
 
-  // Parse file once again for strength reduction
-  const auto strength_reduce_output = SinglePass<std::string>(strength_reducer_transform)(if_convert_output);
-
-  // Expression flattening, recurse to fixed point
-  const auto & expr_flat_output = FixedPointPass<std::string>(ExprFlattenerHandler::transform)(strength_reduce_output);
-
-  TransformVector transforms = { expr_prop_transform, stateful_flank_transform, ssa_transform, partitioning_transform };
-  std::cout << std::accumulate(transforms.begin(), transforms.end(), expr_flat_output, [] (const auto & current_output, const auto & transform)
-                               { return SinglePass<std::string>(transform)(current_output); });
+  /// Process them one after the other
+  std::cout << std::accumulate(transforms.begin(), transforms.end(), string_to_parse, [] (const auto & current_output, const auto & transform)
+                               { return transform.first == TransformType::SINGLE ?
+                                        SinglePass<std::string>(transform.second)(current_output) :
+                                        FixedPointPass<std::string>(transform.second)(current_output); });
 
   return 0;
 }
