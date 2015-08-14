f401285e567e924336101f29bc96d8798a644867
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-14 11:26:42 -0700

    Try-catch block in main to ensure TempFile destructors are called when something throws

--- banzai_code_gen_test.cc
+++ banzai_code_gen_test.cc
@@ -1,6 +1,7 @@
 /// Simple test program to test the Banzai code generator
 #include "banzai_code_generator.h"
 
+#include <exception>
 #include <functional>
 
 #include "util.h"
@@ -9,14 +10,16 @@
 using std::placeholders::_1;
 
 int main(int argc, const char ** argv) {
-  // Get string that needs to be parsed
-  const auto string_to_parse = file_to_str(get_file_name(argc, argv));
+  try {
+    // Get string that needs to be parsed
+    const auto string_to_parse = file_to_str(get_file_name(argc, argv));
 
-  TransformVector transforms;
-  transforms.emplace_back(std::make_unique<SinglePass>(std::bind(& BanzaiCodeGenerator::transform_translation_unit, BanzaiCodeGenerator(), _1)));
+    TransformVector transforms;
+    transforms.emplace_back(std::make_unique<SinglePass>(std::bind(& BanzaiCodeGenerator::transform_translation_unit, BanzaiCodeGenerator(), _1)));
 
-  std::cout << std::accumulate(transforms.begin(), transforms.end(), string_to_parse, [] (const auto & current_output, const auto & transform)
+    std::cout << std::accumulate(transforms.begin(), transforms.end(), string_to_parse, [] (const auto & current_output, const auto & transform)
                                { return (*transform)(current_output); }) << std::endl;
-
-
+  } catch (const std::exception & e) {
+   std::cerr << "Caught exception in main " << std::endl << e.what() << std::endl;
+  }
 }
