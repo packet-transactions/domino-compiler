d66832fc5ea2cb7a4ba16944b11bcc43f34b6703
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-14 15:20:22 -0700

    Shell out to g++ to generate shared library

--- banzai_code_gen_test.cc
+++ banzai_code_gen_test.cc
@@ -4,6 +4,10 @@
 #include <exception>
 #include <functional>
 
+#include "third_party/temp_file.hh"
+#include "third_party/system_runner.hh"
+
+#include "config.h"
 #include "util.h"
 #include "compiler_pass.h"
 
@@ -17,8 +21,18 @@ int main(int argc, const char ** argv) {
     TransformVector transforms;
     transforms.emplace_back(std::make_unique<SinglePass>(std::bind(& BanzaiCodeGenerator::transform_translation_unit, BanzaiCodeGenerator(), _1)));
 
-    std::cout << std::accumulate(transforms.begin(), transforms.end(), string_to_parse, [] (const auto & current_output, const auto & transform)
-                               { return (*transform)(current_output); }) << std::endl;
+    TempFile banzai_file("/tmp/banzai_prog", ".cc");
+    banzai_file.write(std::accumulate(transforms.begin(), transforms.end(), string_to_parse, [] (const auto & current_output, const auto & transform)
+                                      { return (*transform)(current_output); }));
+
+    /// Compiler banzai_file into a .o file
+    TempFile object_file("/tmp/banzai_obj", ".o");
+    run({GPLUSPLUS, "-std=c++14", "-pedantic", "-Wconversion", "-Wsign-conversion", "-Wall", "-Wextra", "-Weffc++", "-Werror", "-fno-default-inline", "-g", "-c", banzai_file.name(), "-fPIC", "-DPIC", "-o", object_file.name()});
+
+    /// Turn that into a shared library
+    UniqueFile library_file("./libbanzai", ".so");
+    run({GPLUSPLUS, "-shared", "-o", library_file.name(), object_file.name()});
+
   } catch (const std::exception & e) {
    std::cerr << "Caught exception in main " << std::endl << e.what() << std::endl;
   }
