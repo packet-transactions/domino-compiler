f4d586d017705beacf8681b96354c94c25b29d17
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-18 11:29:35 -0700

    Move a lot of boilerplate into a function

--- tests/if_converter_test.cc
+++ tests/if_converter_test.cc
@@ -9,33 +9,29 @@
 
 using std::placeholders::_1;
 
-TEST(IfConverterTests, Trivial) {
+void test_if_converter(const std::string & input, const std::string & expected_output) {
   auto if_converter_pass = SinglePass(std::bind(& IfConversionHandler::transform, IfConversionHandler(), _1));
-  const auto input = "int a;";
   const auto output = if_converter_pass(input);
-  ASSERT_EQ(compare_after_removing_space(output, input), true);
+  std::cerr << "Actual output is " << output << std::endl;
+  std::cerr << "Expected output is " << expected_output << std::endl;
+  ASSERT_EQ(compare_after_removing_space(expected_output, output), true);
+}
+
+TEST(IfConverterTests, Trivial) {
+  test_if_converter("int a;", "int a;");
 }
 
 TEST(IfConverterTests, NoIfStmt) {
-  auto if_converter_pass = SinglePass(std::bind(& IfConversionHandler::transform, IfConversionHandler(), _1));
-  const auto input = "int a; struct Packet { int x; }; void func(struct Packet p) { p.x = 2; }";
-  const auto output = if_converter_pass(input);
-  std::cerr << "output is " << output << std::endl;
-  ASSERT_EQ(compare_after_removing_space("int a; struct Packet { int x; }; void func(struct Packet p) { p.x = (1 ? (2) : p.x); }", output), true);
+  test_if_converter("int a; struct Packet { int x; }; void func(struct Packet p) { p.x = 2; }",
+                    "int a; struct Packet { int x; }; void func(struct Packet p) { p.x = (1 ? (2) : p.x); }");
 }
 
 TEST(IfConverterTests, OneIfStmt) {
-  auto if_converter_pass = SinglePass(std::bind(& IfConversionHandler::transform, IfConversionHandler(), _1));
-  const auto input = "int a; struct Packet { int x; }; void func(struct Packet p) { if(p.x) p.x = 2; }";
-  const auto output = if_converter_pass(input);
-  std::cerr << "output is " << output << std::endl;
-  ASSERT_EQ(compare_after_removing_space("int a; struct Packet { int x; int tmp0; }; void func(struct Packet p) { p.tmp0 = (1 ? (p.x) : p.tmp0); p.x = ((1 && p.tmp0) ? (2) : p.x); }", output), true);
+  test_if_converter("int a; struct Packet { int x; }; void func(struct Packet p) { if(p.x) p.x = 2; }",
+                    "int a; struct Packet { int x; int tmp0; }; void func(struct Packet p) { p.tmp0 = (1 ? (p.x) : p.tmp0); p.x = ((1 && p.tmp0) ? (2) : p.x); }");
 }
 
 TEST(IfConverterTests, IfElseStmt) {
-  auto if_converter_pass = SinglePass(std::bind(& IfConversionHandler::transform, IfConversionHandler(), _1));
-  const auto input = "struct Packet {int x;}; void func(struct Packet p) { if(p.x) p.x = 2; else p.x = 3;}";
-  const auto output = if_converter_pass(input);
-  std::cerr << "output is " << output << std::endl;
-  ASSERT_EQ(compare_after_removing_space("struct Packet { int x; int tmp0; }; void func(struct Packet p) { p.tmp0 = (1 ? (p.x) : p.tmp0); p.x = ((1 && p.tmp0) ? (2) : p.x); p.x = ((1 && ! p.tmp0) ? (3) : p.x); }", output), true);
+  test_if_converter("struct Packet {int x;}; void func(struct Packet p) { if(p.x) p.x = 2; else p.x = 3;}",
+                    "struct Packet { int x; int tmp0; }; void func(struct Packet p) { p.tmp0 = (1 ? (p.x) : p.tmp0); p.x = ((1 && p.tmp0) ? (2) : p.x); p.x = ((1 && ! p.tmp0) ? (3) : p.x); }");
 }
