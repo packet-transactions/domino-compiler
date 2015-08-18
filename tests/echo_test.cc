efd7d5e5821c4928ae2327103afb9b24e2ecc607
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-17 22:12:33 -0700

    Simplest possible test for the compiler

--- tests/echo_test.cc
+++ tests/echo_test.cc
@@ -1,6 +1,11 @@
 #include "gtest.h"
 #include "../compiler_pass.h"
+#include "../clang_utility_functions.h"
+#include "../util.h"
 
 TEST(EchoTests, Echo) {
-  ASSERT_EQ(1, 1);
+  auto echo_pass = SinglePass(clang_decl_printer);
+  const std::string input = "int a;";
+  const std::string output = echo_pass(input);
+  ASSERT_EQ(compare_after_removing_space(output, input), true);
 }
