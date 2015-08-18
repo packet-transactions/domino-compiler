3055c20c26a2e96649846327b0946c831c68d902
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-17 22:51:50 -0700

    IfConverterTest with single if statement
    This is turning out to be harder to write than I thought

--- tests/if_converter_test.cc
+++ tests/if_converter_test.cc
@@ -23,3 +23,11 @@ TEST(IfConverterTests, NoIfStmt) {
   std::cerr << "output is " << output << std::endl;
   ASSERT_EQ(compare_after_removing_space("int a; struct Packet { int x; }; void func(struct Packet p) { p.x = (1 ? (2) : p.x); }", output), true);
 }
+
+TEST(IfConverterTests, OneIfStmt) {
+  auto if_converter_pass = SinglePass(std::bind(& IfConversionHandler::transform, IfConversionHandler(), _1));
+  const auto input = "int a; struct Packet { int x; }; void func(struct Packet p) { if(p.x) p.x = 2; }";
+  const auto output = if_converter_pass(input);
+  std::cerr << "output is " << output << std::endl;
+  ASSERT_EQ(compare_after_removing_space("int a; struct Packet { int x; int tmp0; }; void func(struct Packet p) { p.tmp0 = (1 ? (p.x) : p.tmp0); p.x = ((1 && p.tmp0) ? (2) : p.x); }", output), true);
+}
