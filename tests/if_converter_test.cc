cb22123881cc989265bd991cd9fceac76616305a
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-17 22:45:13 -0700

    Add if_converter test with single non-if statement

--- tests/if_converter_test.cc
+++ tests/if_converter_test.cc
@@ -15,3 +15,11 @@ TEST(IfConverterTests, Trivial) {
   const auto output = if_converter_pass(input);
   ASSERT_EQ(compare_after_removing_space(output, input), true);
 }
+
+TEST(IfConverterTests, NoIfStmt) {
+  auto if_converter_pass = SinglePass(std::bind(& IfConversionHandler::transform, IfConversionHandler(), _1));
+  const auto input = "int a; struct Packet { int x; }; void func(struct Packet p) { p.x = 2; }";
+  const auto output = if_converter_pass(input);
+  std::cerr << "output is " << output << std::endl;
+  ASSERT_EQ(compare_after_removing_space("int a; struct Packet { int x; }; void func(struct Packet p) { p.x = (1 ? (2) : p.x); }", output), true);
+}
