4c7a9fdddb4dc27d9db42df2553117be7d42ea54
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-22 16:41:20 -0700

    Add meter example. This is almost certainly unimplementable:
    the atomic section is too big.

---
+++ examples/meter.c
@@ -0,0 +1,38 @@
+#define PBS 100
+#define CBS 10
+
+#define PIR 2
+#define CIR 1
+
+struct Packet {
+  int size;
+  int color;
+  int time;
+};
+
+int tp = PBS;
+int tc = CBS;
+int last_time = 0;
+
+void func(struct Packet pkt) {
+  if (tp < pkt.size) {
+    pkt.color = 1;
+  } else if (tc < pkt.size) {
+    pkt.color = 2;
+    tp = tp - pkt.size;
+  } else if (tc < pkt.size) {
+    pkt.color = 3;
+    tp = tp - pkt.size;
+    tc = tc - pkt.size;
+  }
+
+  // Refill logic
+  tp = tp + PIR * (pkt.time - last_time);
+  if (tp > PBS) tp = PBS;
+
+  tc = tc + CIR * (pkt.time - last_time);
+  if (tc > PBS) tc = CBS;
+
+  last_time = pkt.time;
+
+}
