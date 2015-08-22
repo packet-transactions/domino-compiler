ed09b1843279c935e339087ceecf82e5f048b00f
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-22 14:41:18 -0700

    Learning bloom filter example, compiles with domino

---
+++ examples/learning_bloom_filter.c
@@ -0,0 +1,31 @@
+struct Packet {
+  int sport;
+  int dport;
+  int member;
+};
+
+int filter1[256];
+int filter2[256];
+int filter3[256];
+
+int hash1(int a, int b) {
+  return a * b;
+}
+
+int hash2(int a, int b) {
+  return a * b;
+}
+
+int hash3(int a, int b) {
+  return a * b;
+}
+
+void func(struct Packet pkt) {
+  pkt.member = (filter1[hash1(pkt.sport, pkt.dport)] &&
+                filter2[hash2(pkt.sport, pkt.dport)] &&
+                filter3[hash3(pkt.sport, pkt.dport)]);
+
+  filter1[hash1(pkt.sport, pkt.dport)] = 1;
+  filter2[hash2(pkt.sport, pkt.dport)] = 1;
+  filter3[hash3(pkt.sport, pkt.dport)] = 1;
+}
