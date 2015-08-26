0530baf0eb80a9991c58c2376bbb643949b213fc
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-25 22:37:46 -0700

    Simplify example. Now, it runs on banzai.
    I think we need to handle types sooner or later because we are running
    into all kinds of integer overflows from converting between signed and
    unsigned integers.

--- examples/learning_bloom_filter.c
+++ examples/learning_bloom_filter.c
@@ -8,24 +8,18 @@ int filter1[256] = {1};
 int filter2[256] = {1};
 int filter3[256] = {1};
 
-int hash1(int a, int b) {
-  return a * b;
-}
-
-int hash2(int a, int b) {
-  return a * b;
-}
-
-int hash3(int a, int b) {
-  return a * b;
+int hash(int a, int b) {
+  int ret = (a * b) % 256;
+  if (ret < 0) ret = 0;
+  return ret;
 }
 
 void func(struct Packet pkt) {
-  pkt.member = (filter1[hash1(pkt.sport, pkt.dport)] &&
-                filter2[hash2(pkt.sport, pkt.dport)] &&
-                filter3[hash3(pkt.sport, pkt.dport)]);
+  pkt.member = (filter1[hash(pkt.sport, pkt.dport)] &&
+                filter2[hash(pkt.sport, pkt.dport)] &&
+                filter3[hash(pkt.sport, pkt.dport)]);
 
-  filter1[hash1(pkt.sport, pkt.dport)] = 1;
-  filter2[hash2(pkt.sport, pkt.dport)] = 1;
-  filter3[hash3(pkt.sport, pkt.dport)] = 1;
+  filter1[hash(pkt.sport, pkt.dport)] = 1;
+  filter2[hash(pkt.sport, pkt.dport)] = 1;
+  filter3[hash(pkt.sport, pkt.dport)] = 1;
 }
