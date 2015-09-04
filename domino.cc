31c91d78a9cda83e484d4468221fc8d51bc8e165
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-09-04 11:22:41 -0700

    Allow us to run the same pass multiple times, fixes #60

--- domino.cc
+++ domino.cc
@@ -29,43 +29,45 @@ using std::placeholders::_2;
 
 // Convenience typedefs
 typedef std::string PassName;
-typedef std::map<PassName, std::unique_ptr<CompilerPass>> PassMap;
+typedef std::function<std::unique_ptr<CompilerPass>(void)> PassFunctor;
+typedef std::map<PassName, PassFunctor> PassFactory;
+typedef std::vector<PassFunctor> PassFunctorVector;
 
-// Map to store all passes
-static  PassMap all_passes;
+// Map to store factory for all passes
+static  PassFactory all_passes;
 
 void populate_passes() {
   // We need to explicitly call populate_passes instead of using an initializer list
   // to populate PassMap all_passes because initializer lists don't play well with move-only
   // types like unique_ptrs (http://stackoverflow.com/questions/9618268/initializing-container-of-unique-ptrs-from-initializer-list-fails-with-gcc-4-7)
-  all_passes["int_type_checker"] = std::make_unique<SinglePass>(int_type_checker_transform);
-  all_passes["desugar_comp_asgn"]= std::make_unique<SinglePass>(desugar_compound_assignment_transform);
-  all_passes["if_converter"]     = std::make_unique<SinglePass>(std::bind(& IfConversionHandler::transform, IfConversionHandler(), _1));
-  all_passes["algebra_simplify"] = std::make_unique<SinglePass>(algebraic_simplifier_transform);
-  all_passes["strength_reducer"] = std::make_unique<SinglePass>(strength_reducer_transform);
-  all_passes["expr_flattener"]   = std::make_unique<FixedPointPass>(std::bind(& ExprFlattenerHandler::transform, ExprFlattenerHandler(), _1));
-  all_passes["expr_propagater"]  = std::make_unique<SinglePass>(expr_prop_transform);
-  all_passes["stateful_flanks"]  = std::make_unique<SinglePass>(stateful_flank_transform);
-  all_passes["ssa"]              = std::make_unique<SinglePass>(ssa_transform);
-  all_passes["partitioning"]     = std::make_unique<SinglePass>(partitioning_transform);
-  all_passes["banzai_source"]    = std::make_unique<SinglePass>(std::bind(& BanzaiCodeGenerator::transform_translation_unit, BanzaiCodeGenerator(BanzaiCodeGenerator::CodeGenerationType::SOURCE), _1));
-  all_passes["banzai_binary"]    = std::make_unique<SinglePass>(std::bind(& BanzaiCodeGenerator::transform_translation_unit, BanzaiCodeGenerator(BanzaiCodeGenerator::CodeGenerationType::BINARY), _1));
-  all_passes["echo"]             = std::make_unique<SinglePass>(clang_decl_printer);
-  all_passes["gen_pkt_fields"]   = std::make_unique<SinglePass>(gen_pkt_fields);
+  all_passes["int_type_checker"] = [] () { return std::make_unique<SinglePass>(int_type_checker_transform); };
+  all_passes["desugar_comp_asgn"]= [] () { return std::make_unique<SinglePass>(desugar_compound_assignment_transform); };
+  all_passes["if_converter"]     = [] () { return std::make_unique<SinglePass>(std::bind(& IfConversionHandler::transform, IfConversionHandler(), _1)); };
+  all_passes["algebra_simplify"] = [] () { return std::make_unique<SinglePass>(algebraic_simplifier_transform); };
+  all_passes["strength_reducer"] = [] () { return std::make_unique<SinglePass>(strength_reducer_transform); };
+  all_passes["expr_flattener"]   = [] () { return std::make_unique<FixedPointPass>(std::bind(& ExprFlattenerHandler::transform, ExprFlattenerHandler(), _1)); };
+  all_passes["expr_propagater"]  = [] () { return std::make_unique<SinglePass>(expr_prop_transform); };
+  all_passes["stateful_flanks"]  = [] () { return std::make_unique<SinglePass>(stateful_flank_transform); };
+  all_passes["ssa"]              = [] () { return std::make_unique<SinglePass>(ssa_transform); };
+  all_passes["partitioning"]     = [] () { return std::make_unique<SinglePass>(partitioning_transform); };
+  all_passes["banzai_source"]    = [] () { return std::make_unique<SinglePass>(std::bind(& BanzaiCodeGenerator::transform_translation_unit, BanzaiCodeGenerator(BanzaiCodeGenerator::CodeGenerationType::SOURCE), _1)); };
+  all_passes["banzai_binary"]    = [] () { return std::make_unique<SinglePass>(std::bind(& BanzaiCodeGenerator::transform_translation_unit, BanzaiCodeGenerator(BanzaiCodeGenerator::CodeGenerationType::BINARY), _1)); };
+  all_passes["echo"]             = [] () { return std::make_unique<SinglePass>(clang_decl_printer); };
+  all_passes["gen_pkt_fields"]   = [] () { return std::make_unique<SinglePass>(gen_pkt_fields); };
 }
 
-CompilerPass * get_pass(const std::string & pass_name, const PassMap & pass_map) {
-  if (pass_map.find(pass_name) != pass_map.end()) {
+PassFunctor get_pass_functor(const std::string & pass_name, const PassFactory & pass_factory) {
+  if (pass_factory.find(pass_name) != pass_factory.end()) {
     // This is a bit misleading because we don't have deep const correctness
-    return pass_map.at(pass_name).get();
+    return pass_factory.at(pass_name);
   } else {
     throw std::logic_error("Unknown pass " + pass_name);
   }
 }
 
-std::string all_passes_as_string(const PassMap & pass_map) {
+std::string all_passes_as_string(const PassFactory & pass_factory) {
   std::string ret;
-  for (const auto & pass_pair : pass_map)
+  for (const auto & pass_pair : pass_factory)
     ret += pass_pair.first + "\n";
   return ret;
 }
@@ -87,12 +89,12 @@ int main(int argc, const char **argv) {
       pass_list = split(std::string(argv[2]), ",");
 
       // add all user-requested passes in the same order that the user specified
-      TransformVector transforms;
-      for (const auto & pass_name : pass_list) transforms.emplace_back(get_pass(pass_name, all_passes));
+      PassFunctorVector passes_to_run;
+      for (const auto & pass_name : pass_list) passes_to_run.emplace_back(get_pass_functor(pass_name, all_passes));
 
       /// Process them one after the other
-      std::cout << std::accumulate(transforms.begin(), transforms.end(), string_to_parse, [] (const auto & current_output, const auto & transform)
-                                   { return (*transform)(current_output); });
+      std::cout << std::accumulate(passes_to_run.begin(), passes_to_run.end(), string_to_parse, [] (const auto & current_output, const auto & pass_functor __attribute__((unused)))
+                                   { return (*pass_functor())(current_output); });
 
       return EXIT_SUCCESS;
     }
