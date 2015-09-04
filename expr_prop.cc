a65f6aa1289998ca505e2d59a489a38bd45c723c
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-09-04 11:27:22 -0700

    We can't propagate array expressions either, similar to d69c78867e5d0461dac6767552938fa86f859970

--- expr_prop.cc
+++ expr_prop.cc
@@ -31,13 +31,14 @@ std::pair<std::string, std::vector<std::string>> expr_prop_fn_body(const Compoun
     // Populate var_to_expr, so long as
     // 1. The lhs variable doesn't appear within the rhs expression.
     // 2. The rhs expression isn't a (or doesn't contain a) DeclRefExpr
-    // We don't want to be propagating DeclRefExpr's because DeclRefExpr are
+    // We don't want to be propagating DeclRefExpr's or ArraySubscriptExpr because these are
     // the syntactic construct for state variables and propagating state variables
     // destroys the property that state variables are only ever read at the top of the program
     // The partitiioning pass relies on this property.
     const auto & var_list = gen_var_list(rhs);
     if (var_list.find(clang_stmt_printer(lhs)) == var_list.end() and
-        (not isa<DeclRefExpr>(rhs))) {
+        (not isa<DeclRefExpr>(rhs)) and
+        (not isa<ArraySubscriptExpr>(rhs))) {
       var_to_expr[clang_stmt_printer(lhs)] = clang_stmt_printer(rhs);
     }
 
