5250358f194d030182eb50f43bc25f1802935fc9
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-09-05 14:02:38 -0700

    Need to modulo the actual number of hops

--- examples/flowlet_switching_simple.c
+++ examples/flowlet_switching_simple.c
@@ -1,6 +1,7 @@
 #include "hashes.h"
 #define NUM_FLOWLETS      8000
 #define FLOWLET_THRESHOLD 5
+#define NUM_HOPS 64
 
 struct Packet {
   int sport;
@@ -16,7 +17,7 @@ int last_time [NUM_FLOWLETS] = {0};
 int saved_hop [NUM_FLOWLETS] = {0};
 
 void flowlet(struct Packet pkt) {
-  pkt.new_hop   = hash3(pkt.sport, pkt.dport, pkt.arrival_time);
+  pkt.new_hop   = hash3(pkt.sport, pkt.dport, pkt.arrival_time) % NUM_HOPS;
   pkt.last_time_idx = hash2(pkt.sport, pkt.dport) % NUM_FLOWLETS;
   pkt.saved_hop_idx = hash2(pkt.sport, pkt.dport) % NUM_FLOWLETS;
   if (pkt.arrival_time - last_time[pkt.last_time_idx] >
