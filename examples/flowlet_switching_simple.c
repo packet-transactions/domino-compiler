6c3184c5e2c1568a76cad0a9654ff1efcc0af441
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-09-07 17:55:23 -0700

    Update flowlet_switching_simple.c to what is in the paper

--- examples/flowlet_switching_simple.c
+++ examples/flowlet_switching_simple.c
@@ -1,29 +1,42 @@
 #include "hashes.h"
+
 #define NUM_FLOWLETS      8000
 #define FLOWLET_THRESHOLD 5
-#define NUM_HOPS 64
+#define NUM_HOPS          10
 
 struct Packet {
   int sport;
   int dport;
   int new_hop;
-  int arrival_time;
+  int arrival;
   int next_hop;
-  int id0;
-  int id1;
+  int idl; // index into last_time
+  int ids; // index into saved_hop
 };
 
 int last_time [NUM_FLOWLETS] = {0};
 int saved_hop [NUM_FLOWLETS] = {0};
 
 void flowlet(struct Packet pkt) {
-  pkt.new_hop   = hash3(pkt.sport, pkt.dport, pkt.arrival_time) % NUM_HOPS;
-  pkt.id1 = hash2(pkt.sport, pkt.dport) % NUM_FLOWLETS;
-  pkt.id0 = hash2(pkt.sport, pkt.dport) % NUM_FLOWLETS;
-  if (pkt.arrival_time - last_time[pkt.id1] >
+  pkt.new_hop = hash3(pkt.sport,
+                      pkt.dport,
+                      pkt.arrival);
+                % NUM_HOPS;
+
+  pkt.idl = hash2(pkt.sport,
+                  pkt.dport)
+            % NUM_FLOWLETS;
+
+  pkt.ids = hash2(pkt.sport,
+                  pkt.dport)
+            % NUM_FLOWLETS;
+
+  if (pkt.arrival_time -
+      last_time[pkt.idl] >
       FLOWLET_THRESHOLD) {
-    saved_hop[pkt.id0] = pkt.new_hop;
+    saved_hop[pkt.ids] = pkt.new_hop;
   }
-  last_time[pkt.id1] = pkt.arrival_time;
-  pkt.next_hop = saved_hop[pkt.id0];
+
+  last_time[pkt.idl] = pkt.arrival;
+  pkt.next_hop = saved_hop[pkt.ids];
 }
