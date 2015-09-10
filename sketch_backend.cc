86c9d0fcde080a1d5a2e56025a55add16092389d
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-09-10 15:33:00 -0700

    Don't add ref for packet arguments

--- sketch_backend.cc
+++ sketch_backend.cc
@@ -81,9 +81,13 @@ std::string create_sketch_spec(const Stmt * function_body, const std::string & s
   // Add function signature
   std::string sketch_spec_signature = "void " + spec_name + "(";
   bool empty = true;
-  for (const auto & arg : state_var_args + pkt_var_args) {
+  for (const auto & arg : state_var_args) {
     empty = false;
     sketch_spec_signature += "ref int " + arg + ",";
+  }
+  for (const auto & arg : pkt_var_args) {
+    empty = false;
+    sketch_spec_signature += "int " + arg + ",";
   }
   if (empty) sketch_spec_signature += ")";
   else sketch_spec_signature.back() = ')';
