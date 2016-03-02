67f496ffb228c184c6cccf3c8c5b6ff17d424d76
Anirudh Sivaraman <sk.anirudh@gmail.com>
2016-03-02 17:31:13 -0500

    Cosmetic fixes, audit code once

--- pytoc.py
+++ pytoc.py
@@ -1,17 +1,19 @@
 #! /usr/bin/python
-from ast import NodeVisitor
-from ast import parse
-from ast import dump
-from ast import Name
-from ast import Subscript
-from ast import FunctionDef
-from ast import Assign
-from ast import Num
-from sys import argv
-from io  import StringIO
+from ast     import NodeVisitor
+from ast     import parse
+from ast     import dump
+from ast     import Name
+from ast     import Subscript
+from ast     import FunctionDef
+from ast     import Assign
+from ast     import Num
+from sys     import argv
+from io      import StringIO
 from unparse import Unparser
 
-# Check if target is a state variable (scalar/Name or array/Subscript)
+# Extract name of state variable from either
+# 1. scalar state variable (Name) or
+# 2. state variable array (Subscript)
 def track_state_vars(target):
   if type(target) is Name:
     return target.id
@@ -20,7 +22,7 @@ def track_state_vars(target):
   else:
     return None
 
-# Track reads and writes to state (global) variables
+# Track reads and writes to state variables in function body
 class StateReadWrite(NodeVisitor):
   def __init__(self):
     self.write_set = set()
@@ -28,7 +30,7 @@ class StateReadWrite(NodeVisitor):
     self.write_set.add(None)
     self.read_set.add(None)
 
-  # Writes
+  # Writes (Assign and AugAssign)
   def visit_Assign(self, node):
     for target in node.targets:
       self.write_set.add(track_state_vars(target))
@@ -36,18 +38,22 @@ class StateReadWrite(NodeVisitor):
     for target in node.targets:
       self.write_set.add(track_state_vars(target))
 
-  # Reads
+  # Reads (scalars and arrays)
   def visit_Name(self, node):
     self.read_set.add(node.id)
   def visit_Subscript(self, node):
     self.read_set.add(node.value.id) 
 
+# Collect fields used in function body
 class FieldCollector(NodeVisitor):
   def __init__(self):
     self.field_set = set()
   def visit_Attribute(self, node):
     self.field_set.add(node.attr)
 
+# The core logic,
+# go through the function body,
+# identify state variables and packet fields
 class PyToC(NodeVisitor):
   def __init__(self):
     self.read_set = set()
@@ -56,7 +62,7 @@ class PyToC(NodeVisitor):
     self.program_output = StringIO()
 
   def visit_FunctionDef(self, node):
-    # Track reads and writes to state (global) variables
+    # Compute read and write sets for state variables
     state_read_write = StateReadWrite()
     state_read_write.visit(node)
     self.read_set = state_read_write.read_set
@@ -74,11 +80,16 @@ class PyToC(NodeVisitor):
     for stmt in node.body:
       self.visit(stmt)
     print("}", file = self.program_output)
+
+  # Visitors for all other nodes traversed recursively from FunctionDef
+  # Use Unparser to turn nodes into text
   def visit_Compare(self, node):
     Unparser(node, self.program_output)
+
   def visit_Assign(self, node):
     Unparser(node, self.program_output)
     print(";\n", file = self.program_output)
+
   def visit_If(self, node):
     print("if ", file = self.program_output)
     self.visit(node.test)
@@ -90,6 +101,7 @@ class PyToC(NodeVisitor):
       self.visit(stmt)
     print("}", file = self.program_output)
 
+# Main function in the program
 # Parse the top level and generate nodes
 # corresponding to
 # 1. state assignments, and
@@ -105,6 +117,7 @@ for node in top_level.body:
       state_assignments[target.id] = node.value
   else:
     assert(False)
+# There can be only a single function definition
 assert(len(function_def_nodes) == 1)
 
 # Parse the function alone and generate all the data structures we need
@@ -114,11 +127,11 @@ pytoc_converter.visit(function_def_nodes[0])
 # Now print out the converted output
 # Print out state
 for state in state_assignments:
-  # Constant state
+  # Convert constant state into C macros
   if (state in pytoc_converter.read_set and state not in pytoc_converter.write_set):
     assert(type(state_assignments[state]) is Num)
     print("#define ", state, state_assignments[state].n);
-  # Mutable state
+  # Convert mutable state into C arrays or scalars
   elif (state in pytoc_converter.write_set):
     assert(type(state_assignments[state]) is Num)
     print("int ", state, " = ", state_assignments[state].n, ";");
