9f4f09a74bcf194d626dab48dcaa47d451752ab0
Xiangyu Gao <xg673@nyu.edu>
2019-03-13 20:13:54 -0400

    Use domino_to_rename_domino to rename stateful and stateless vars

---
+++ domino_to_rename_domino.cc
@@ -0,0 +1,50 @@
+#include <csignal>
+#include "rename_domino_code_generator.h"
+
+#include <utility>
+#include <iostream>
+#include <set>
+#include <string>
+#include <functional>
+
+#include "third_party/assert_exception.h"
+
+#include "util.h"
+#include "pkt_func_transform.h"
+#include "compiler_pass.h"
+
+// For the _1, and _2 in std::bind
+// (Partial Function Application)
+using std::placeholders::_1;
+
+void print_usage() {
+  std::cerr << "Usage: domino_to_rename_domino <source_file>" << std::endl;
+}
+
+int main(int argc, const char **argv) {
+  try {
+    // Block out SIGINT, because we can't handle it properly
+    signal(SIGINT, SIG_IGN);
+
+    if (argc == 2) {
+      const auto string_to_parse = file_to_str(std::string(argv[1]));
+
+      auto rename_domino_code_generator = SinglePass<>(std::bind(& RenameDominoCodeGenerator::ast_visit_transform,
+                                                  RenameDominoCodeGenerator(), _1));
+
+      std::cout << "/* \n// Original program: \n" + string_to_parse + " */\n" << std::endl;
+      
+      std::string sketch_program = rename_domino_code_generator(string_to_parse);
+      std::cout << sketch_program << std::endl;
+      
+      return EXIT_SUCCESS;
+    }
+    else {
+      print_usage();
+      return EXIT_FAILURE;
+    }
+  } catch (const std::exception & e) {
+    std::cerr << "Caught exception in main " << std::endl << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+}
---
+++ rename_domino_code_generator.cc
@@ -0,0 +1,86 @@
+#include "rename_domino_code_generator.h"
+
+#include "third_party/assert_exception.h"
+#include "clang_utility_functions.h"
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <algorithm>
+
+using namespace clang;
+
+std::string RenameDominoCodeGenerator::ast_visit_transform(const clang::TranslationUnitDecl * tu_decl) {
+  // TODO: Need to check if we have more than one packet func per tu_decl and report an error if so.
+  for (const auto * decl : dyn_cast<DeclContext>(tu_decl)->decls()) {
+    if (isa<FunctionDecl>(decl) and (is_packet_func(dyn_cast<FunctionDecl>(decl)))) {
+      //record body part first
+      std::string body_part = ast_visit_stmt(dyn_cast<FunctionDecl>(decl)->getBody());
+      return body_part;
+    }
+  }
+  assert_exception(false);
+}
+
+std::string RenameDominoCodeGenerator::ast_visit_decl_ref_expr(const clang::DeclRefExpr * decl_ref_expr) {
+  assert_exception(decl_ref_expr);
+  std::string s = clang_stmt_printer(decl_ref_expr);
+  std::map<std::string,std::string>::iterator it;
+        it = c_to_sk.find(s);
+        if (it == c_to_sk.end()){
+            std::string name;
+            //stateless
+	    if (s.find('.')!=std::string::npos){
+		// Should never get here.
+		assert_exception(false);
+	    }
+	    else{
+                name = "state_" + std::to_string(count_stateful);
+                count_stateful++;
+		c_to_sk[s] = name;
+	    }
+        }
+  return c_to_sk[s];
+}
+
+std::string RenameDominoCodeGenerator::ast_visit_member_expr(const clang::MemberExpr * member_expr) {
+  assert_exception(member_expr);
+  std::string s = clang_stmt_printer(member_expr);
+  std::map<std::string,std::string>::iterator it;
+        it = c_to_sk.find(s);
+        if (it == c_to_sk.end()){
+            std::string name;
+            //stateless
+            if (s.find('.')!=std::string::npos && s.find('[')==std::string::npos){
+                name = "pkt_" + std::to_string(count_stateless);
+                count_stateless++;
+            }
+            else{
+                // Should never get here.
+                assert_exception(false);
+            }
+            c_to_sk[s] = name;
+        }
+  return c_to_sk[s];
+}
+
+std::string RenameDominoCodeGenerator::ast_visit_array_subscript_expr(const clang::ArraySubscriptExpr * array_subscript_expr){
+  assert_exception(array_subscript_expr);
+  std::string s = clang_stmt_printer(array_subscript_expr);
+
+  std::map<std::string,std::string>::iterator it;
+        it = c_to_sk.find(s);
+        if (it == c_to_sk.end()){
+            std::string name;
+  	    if (s.find('[')==std::string::npos){
+		// Should never get here.
+		assert_exception(false);
+	    }else{
+            //stateless
+            name = "state_" + std::to_string(count_stateful);
+            count_stateful++;
+            c_to_sk[s] = name;
+	    }
+        }
+  return c_to_sk[s];
+}
