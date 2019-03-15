d6da90098f98ac7da850bf1ec8e136e79ad4e7a4
Xiangyu Gao <xg673@nyu.edu>
2019-03-15 10:45:46 -0400

    Add two files

---
+++ domino_to_group_domino_code_gen.cc
@@ -0,0 +1,35 @@
+#include "domino_to_group_domino_code_gen.h"
+
+#include "third_party/assert_exception.h"
+#include "clang_utility_functions.h"
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <algorithm>
+
+using namespace clang;
+
+std::string DominoToGroupDominoCodeGenerator::ast_visit_transform(const clang::TranslationUnitDecl * tu_decl){
+  // TODO: Need to check if we have more than one packet func per tu_decl and report an error if so.
+  for (const auto * decl : dyn_cast<DeclContext>(tu_decl)->decls()) {
+    if (isa<FunctionDecl>(decl) and (is_packet_func(dyn_cast<FunctionDecl>(decl)))) {
+      //record body part first
+      std::string body_part = ast_visit_stmt(dyn_cast<FunctionDecl>(decl)->getBody());
+      return body_part;
+    }
+  }
+  assert_exception(false);
+}
+
+std::string DominoToGroupDominoCodeGenerator::ast_visit_decl_ref_expr(const clang::DeclRefExpr * decl_ref_expr) {
+  assert_exception(decl_ref_expr);
+  std::string s = clang_stmt_printer(decl_ref_expr);
+  std::map<std::string,std::string>::iterator it;
+        it = c_to_sk.find(s);
+        if (it != c_to_sk.end()){
+           return c_to_sk[s];
+        }else
+           return s;
+}
+
