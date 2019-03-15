cdf798daded7b7562d7cd4772247baa6d93374ca
Xiangyu Gao <xg673@nyu.edu>
2019-03-15 00:23:00 -0400

    Modify rename_domino_code_generator so that we can run domino_to_rename_domino

--- rename_domino_code_generator.cc
+++ rename_domino_code_generator.cc
@@ -11,12 +11,31 @@
 using namespace clang;
 
 std::string RenameDominoCodeGenerator::ast_visit_transform(const clang::TranslationUnitDecl * tu_decl) {
+  //TODO: First pass to set up the map
+  for (const auto * decl : dyn_cast<DeclContext>(tu_decl)->decls()) {
+    if (isa<FunctionDecl>(decl) and (is_packet_func(dyn_cast<FunctionDecl>(decl)))) {
+      std::string body_part = ast_visit_stmt(dyn_cast<FunctionDecl>(decl)->getBody());
+    }
+  }
   // TODO: Need to check if we have more than one packet func per tu_decl and report an error if so.
   for (const auto * decl : dyn_cast<DeclContext>(tu_decl)->decls()) {
     if (isa<FunctionDecl>(decl) and (is_packet_func(dyn_cast<FunctionDecl>(decl)))) {
       //record body part first
       std::string body_part = ast_visit_stmt(dyn_cast<FunctionDecl>(decl)->getBody());
       return "void func(struct Packet p) {\n" + body_part + "\n}";
+    }else if (isa<VarDecl>(decl) || isa<RecordDecl>(decl)){
+         std::string str = clang_decl_printer(decl);
+         for (std::map<std::string,std::string>::iterator it = c_to_sk.begin();it != c_to_sk.end();it++){
+           size_t start_pos = str.find(it->first);
+           if (start_pos == std::string::npos){
+             continue;
+           }else if (str[start_pos-1]==' '){
+              str.replace(start_pos,it->first.length(),it->second);
+           }
+         }
+           std::cout << str << ";\n";
+    }else if ((isa<FunctionDecl>(decl) and (not is_packet_func(dyn_cast<FunctionDecl>(decl))))) {
+         std::cout << clang_decl_printer(decl) << "\n";
     }
   }
   assert_exception(false);
@@ -46,13 +65,15 @@ std::string RenameDominoCodeGenerator::ast_visit_decl_ref_expr(const clang::Decl
 std::string RenameDominoCodeGenerator::ast_visit_member_expr(const clang::MemberExpr * member_expr) {
   assert_exception(member_expr);
   std::string s = clang_stmt_printer(member_expr);
+  //TODO p.src -> src
+  s=s.substr(s.find('.')+1);
   std::map<std::string,std::string>::iterator it;
         it = c_to_sk.find(s);
         if (it == c_to_sk.end()){
             std::string name;
             //stateless
-            if (s.find('.')!=std::string::npos && s.find('[')==std::string::npos){
-                name = "p.pkt_" + std::to_string(count_stateless);
+            if (s.find('[')==std::string::npos){
+                name = "pkt_" + std::to_string(count_stateless);
                 count_stateless++;
             }
             else{
@@ -61,7 +82,7 @@ std::string RenameDominoCodeGenerator::ast_visit_member_expr(const clang::Member
             }
             c_to_sk[s] = name;
         }
-  return c_to_sk[s];
+  return "p."+c_to_sk[s];
 }
 
 std::string RenameDominoCodeGenerator::ast_visit_array_subscript_expr(const clang::ArraySubscriptExpr * array_subscript_expr){
